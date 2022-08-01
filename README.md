2d4d1a1a8ba3106d6da19a714019b9c257205df0
Jeff <jbridges1119@gmail.com>
2022-08-01 09:36:27 -0600

    Add code for server/db connection and add required dependencies

---
+++ README.md
@@ -0,0 +1,15 @@
+## Getting Started
+Within  /server
+npm i
+
+Within /client
+npm i
+
+
+Create postgres database
+Copy .env.example
+Rename to .env
+Fill in relevant db information
+
+npm run db:reset ~ fill db with tables
+npm start ~ start backend server
---
+++ server/routes/users.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+
+
+//eXAMPLE
+module.exports = (db) => {
+    const example = {}
+    const query = ""
+  router.get("/", (req, res) => {
+    db.query(query, [example])
+    .then(data => {
+      const info = data.rows
+      res.json({info})
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  })
+}
