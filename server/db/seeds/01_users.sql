e1a73923719314f1afb9875f19f118df4d40cf2a
Kenny Moon <kennymoon90@gmail.com>
2022-08-08 11:35:39 -0700

    Add user seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,4 +1,22 @@
 INSERT INTO users (first_name, last_name, email, password, city)
 VALUES ('jeff', 'bridges', 'me@me.com', 'password123', 'lethbridge');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Ken', 'Moon', 'moon@me.com', 'password123', 'Vancouver');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Jesse', 'Kopala', 'kopala@me.com', 'password123', 'Calgary');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Philip', 'Fry', 'fry@me.com', 'password123', 'Vancouver');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Hubert', 'Farnsworth', 'farnsworth@me.com', 'password123', 'Calgary');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('John', 'Zoidberg', 'zoidberg@me.com', 'password123', 'lethbridge');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Bender', 'Rodriguez', 'rodriguez@me.com', 'password123', 'lethbridge');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Turanga', 'Leela', 'leela@me.com', 'password123', 'Calgary');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Amy', 'Wong', 'wong@me.com', 'password123', 'Calgary');
+INSERT INTO users (first_name, last_name, email, password, city)
+VALUES ('Hermes', 'Conrad', 'conrad@me.com', 'password123', 'Vancouver');
 
 
