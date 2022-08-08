71e3f0d0f21b79baa3d8e87d349d012062d53e16
Jeff <jbridges1119@gmail.com>
2022-08-07 23:17:04 -0600

    Move buttons on to seprate component

---
+++ clients/src/pages/NytePage/NytePageComponents/NyteDetailsComponents/statusButtons/Host.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import theme from "../../../../../assets/theme"
+
+const Host = (props) => {
+  return ( 
+ 
+           <Button  sx={{backgroundColor: "darkred", height: 50, width:200, fontSize: 20,  borderRadius: '20px'  }} variant="contained">Cancel event</Button>
+   )
+}
+ 
+export default Host;
