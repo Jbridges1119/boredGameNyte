066f5aef4bdc674f974afa1b1e5d11e282f9e016
Jeff <jbridges1119@gmail.com>
2022-08-07 23:15:06 -0600

    Change attending to be null on default

--- server/db/schema/04_attendees.sql
+++ server/db/schema/04_attendees.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS attendees CASCADE;
 CREATE TABLE attendees (
   attendee_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   game_night_id INTEGER REFERENCES game_nights(id) ON DELETE CASCADE,
-  attend_status BOOLEAN NOT NULL DEFAULT FALSE
+  attend_status BOOLEAN
 );
