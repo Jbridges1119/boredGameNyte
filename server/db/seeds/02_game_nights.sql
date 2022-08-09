fccec0eb998b45a7de09fd53fde03bbbc3e8306b
Kenny Moon <kennymoon90@gmail.com>
2022-08-09 10:40:26 -0700

    Fix host id error

--- server/db/seeds/02_game_nights.sql
+++ server/db/seeds/02_game_nights.sql
@@ -5,16 +5,16 @@ VALUES ('Catan Night at Jeffs', 1, TRUE, 'complete', 'lethbridge', 'Wed July 29 
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('Newbie Night at Jeffs', 1, FALSE, 'complete', 'lethbridge', 'Wed Aug 7 2022 18:00:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Game Night at Jesses', 2, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Game Night at Jesses', 3, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Lords of Waterdeep Game Night', 2, TRUE, 'complete', 'Calgary', 'Wed July 22 2022 19:15:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Lords of Waterdeep Game Night', 3, TRUE, 'complete', 'Calgary', 'Wed July 22 2022 19:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('What's Canada's Favorite board game? Sorry!', 2, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('What's Canada's Favorite board game? Sorry!', 3, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('A Host with no Clue!', 2, TRUE, 'Scheduled', 'Calgary', 'Wed Aug 29 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('A Host with no Clue!', 3, TRUE, 'Scheduled', 'Calgary', 'Wed Aug 29 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Game Night at Kennys', 3, TRUE, 'complete', 'Vancouver', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Game Night at Kennys', 2, TRUE, 'complete', 'Vancouver', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Risk Takers', 3, TRUE, 'complete', 'Vancouver', 'Wed Aug 12 2022 19:45:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Risk Takers', 2, TRUE, 'complete', 'Vancouver', 'Wed Aug 12 2022 19:45:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('A night of Life', 3, FALSE, 'scheduled', 'Vancouver', 'Wed Aug 28 2022 20:00:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('A night of Life', 2, FALSE, 'scheduled', 'Vancouver', 'Wed Aug 28 2022 20:00:00 GMT-0600 (Mountain Daylight Time)');
