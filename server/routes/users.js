00064e426decb686075cf1a4ce48ab30fae4415d
Jesse Kopala <jdkopala@gmail.com>
2022-08-09 15:06:05 -0600

    Implement query to find users friends

--- server/routes/users.js
+++ server/routes/users.js
@@ -28,5 +28,21 @@ module.exports = (db) => {
         return res.json(data.rows[0])
       })
   })
+
+  router.get("/:id/friends", (req, res) => {
+    let user_id = req.params.id;
+    const params = [user_id];
+    const query = `
+    SELECT user_id, friend_id, first_name, last_name, email, city
+    FROM friends
+    JOIN users on id = friend_id
+    WHERE user_id = $1;
+    `
+    return db.query(query, params)
+      .then((data) => {
+        console.log(data.rows)
+        return res.json(data.rows)
+      })
+  })
   return router;
 };
