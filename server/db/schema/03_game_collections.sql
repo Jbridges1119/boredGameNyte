ff5194c6de2d4fa4430efb29ede962303c959add
Jesse Kopala <jdkopala@gmail.com>
2022-08-09 12:44:53 -0600

    Expand game_collections table

--- server/db/schema/03_game_collections.sql
+++ server/db/schema/03_game_collections.sql
@@ -3,5 +3,10 @@ DROP TABLE IF EXISTS game_collections CASCADE;
 CREATE TABLE game_collections (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  name VARCHAR(255),
+  thumb_url VARCHAR(255),
+  large_url VARCHAR(255),
+  min_players smallInt,
+  max_players smallInt,
   bgatlas_game_id VARCHAR(255) NOT NULL
 );
--- server/db/seeds/03_game_collections.sql
+++ server/db/seeds/03_game_collections.sql
@@ -11,49 +11,49 @@
 -- Carcassonne - oGVgRSAKwX
 -- UNO - Ad9NDUFxmt
 
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1,' OIXt3DmJU0');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, 'EL3YmDLY6W');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, 'Ad9NDUFxmt');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, '3RVcHxhPEZ');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, 'oGVgRSAKwX');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, 'RLlDWHh7hR');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (1, 'i5Oqu5VZgP');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Catan', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 3, 4, 'OIXt3DmJU0');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Risk','https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254847937-51zyVWbt8aL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254847937-51zyVWbt8aL.jpg', 2, 6, 'EL3YmDLY6W');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'UNO','https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731709416', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731709416', 2, 10, 'Ad9NDUFxmt');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Betrayal at House on the Hill', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 3, 6, '3RVcHxhPEZ');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Carcasonne', 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/carcassonne-board-game.jpg?v=1609629064', 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/carcassonne-board-game.jpg?v=1609629064', 2, 5, 'oGVgRSAKwX');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Gloomhaven', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254920151-51ulRXlJ7LL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254920151-51ulRXlJ7LL.jpg', 1, 4,'RLlDWHh7hR');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (1, 'Azul', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254200327-61EFZADvURL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254200327-61EFZADvURL.jpg', 2, 4, 'i5Oqu5VZgP');
 
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, 'OIXt3DmJU0');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, 'axOr9uuOwN');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, 'i5Oqu5VZgP');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, 'EL3YmDLY6W');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, '3RVcHxhPEZ');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, '5H5JS0KLzK');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (2, 'RLlDWHh7hR');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Catan', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 3, 4, 'OIXt3DmJU0');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Lords of Waterdeep', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324168873.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324168873.jpg', 2, 5,'axOr9uuOwN');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Azul', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254200327-61EFZADvURL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254200327-61EFZADvURL.jpg', 2, 4, 'i5Oqu5VZgP');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Risk','https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254847937-51zyVWbt8aL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254847937-51zyVWbt8aL.jpg', 2, 6, 'EL3YmDLY6W');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Betrayal at House on the Hill', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 3, 6, '3RVcHxhPEZ');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Wingspan', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629325193747.png', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629325193747.png', 1, 5, '5H5JS0KLzK');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (2, 'Gloomhaven', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254920151-51ulRXlJ7LL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254920151-51ulRXlJ7LL.jpg', 1, 4,'RLlDWHh7hR');
 
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'OIXt3DmJU0');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'axOr9uuOwN');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'Z6h8zF22uh');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'WVgc4bUL8N');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, '3RVcHxhPEZ');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'OF145SrX44');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'oGVgRSAKwX');
-INSERT INTO game_collections (user_id, bgatlas_game_id)
-VALUES (3, 'Ad9NDUFxmt');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Catan', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324722072.jpg', 3, 4, 'OIXt3DmJU0');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Lords of Waterdeep', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324168873.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324168873.jpg', 2, 5,'axOr9uuOwN');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Sorry!', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/empty+box+thumb.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/empty+box.jpg', 2, 4,'Z6h8zF22uh');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Clue', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1600449593257', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1600449593257', 3, 6, 'WVgc4bUL8N');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Betrayal at House on the Hill', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1595268779978', 3, 6, '3RVcHxhPEZ');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, '7 Wonders', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559253945470-61dlm9QcXpL.jpg', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559253945470-61dlm9QcXpL.jpg', 2, 7, 'OF145SrX44');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'Carcasonne', 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/carcassonne-board-game.jpg?v=1609629064', 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/carcassonne-board-game.jpg?v=1609629064', 2, 5, 'oGVgRSAKwX');
+INSERT INTO game_collections (user_id, name, thumb_url, large_url, min_players, max_players, bgatlas_game_id)
+VALUES (3, 'UNO','https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731709416', 'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731709416', 2, 10, 'Ad9NDUFxmt');
