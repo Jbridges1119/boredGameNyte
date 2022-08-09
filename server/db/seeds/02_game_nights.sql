3b013f2db4f13c776735ae205b4cbb181f64e5f3
Jesse Kopala <jdkopala@gmail.com>
2022-08-09 12:01:29 -0600

    sedd update

--- server/db/seeds/02_game_nights.sql
+++ server/db/seeds/02_game_nights.sql
@@ -1,15 +1,15 @@
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Game Night at Jeffs', 1, FALSE, 'complete', 'lethbridge', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Game Night at Jeffs', 1, FALSE, 'complete', 'Lethbridge', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Catan Night at Jeffs', 1, TRUE, 'complete', 'lethbridge', 'Wed July 29 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Catan Night at Jeffs', 1, TRUE, 'complete', 'Lethbridge', 'Wed July 29 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Newbie Night at Jeffs', 1, FALSE, 'complete', 'lethbridge', 'Wed Aug 7 2022 18:00:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Newbie Night at Jeffs', 1, FALSE, 'complete', 'Lethbridge', 'Wed Aug 7 2022 18:00:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('Game Night at Jesses', 3, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('Lords of Waterdeep Game Night', 3, TRUE, 'complete', 'Calgary', 'Wed July 22 2022 19:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('What's Canada's Favorite board game? Sorry!', 3, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
+VALUES ('Whats Canadas Favorite board game? Sorry!', 3, FALSE, 'complete', 'Calgary', 'Wed Aug 2 2022 19:00:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('A Host with no Clue!', 3, TRUE, 'Scheduled', 'Calgary', 'Wed Aug 29 2022 18:15:00 GMT-0600 (Mountain Daylight Time)');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
--- server/db/seeds/04_attendees.sql
+++ server/db/seeds/04_attendees.sql
@@ -1,80 +1,80 @@
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (1, 1, TRUE);
+VALUES (1, 1, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (6, 1, TRUE);
+VALUES (6, 1, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (7, 1, TRUE);
+VALUES (7, 1, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (11, 1 TRUE);
+VALUES (11, 1, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (1, 2, TRUE);
+VALUES (1, 2, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (6, 2, TRUE);
+VALUES (6, 2, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (7, 2, TRUE);
+VALUES (7, 2, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (11, 2, TRUE);
+VALUES (11, 2, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (1, 3, TRUE);
+VALUES (1, 3, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (6, 3, FALSE);
+VALUES (6, 3, 'false');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (7, 3, TRUE);
+VALUES (7, 3, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (11, 3, TRUE);
+VALUES (11, 3, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (3, 4, TRUE);
+VALUES (3, 4, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (5, 4, TRUE);
+VALUES (5, 4, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (8, 4, TRUE);
+VALUES (8, 4, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (9, 4, FALSE);
+VALUES (9, 4, 'false');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (3, 5, TRUE);
+VALUES (3, 5, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (5, 5, TRUE);
+VALUES (5, 5, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (8, 5, TRUE);
+VALUES (8, 5, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (9, 5, TRUE);
+VALUES (9, 5, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (3, 6, TRUE);
+VALUES (3, 6, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (5, 6, TRUE);
+VALUES (5, 6, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (8, 6, TRUE);
+VALUES (8, 6, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (9, 6, TRUE);
+VALUES (9, 6, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (3, 7, TRUE);
+VALUES (3, 7, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (5, 7, TRUE);
+VALUES (5, 7, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (8, 7, TRUE);
+VALUES (8, 7, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (9, 7, TRUE);
+VALUES (9, 7, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (2, 8, TRUE);
+VALUES (2, 8, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (4, 8, TRUE);
+VALUES (4, 8, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (10, 8, TRUE);
+VALUES (10, 8, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (12, 8, TRUE);
+VALUES (12, 8, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (2, 9, TRUE);
+VALUES (2, 9, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (4, 9, TRUE);
+VALUES (4, 9, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (10, 9, TRUE);
+VALUES (10, 9, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (12, 9, TRUE);
+VALUES (12, 9, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (2, 10, TRUE);
+VALUES (2, 10, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (4, 10, TRUE);
+VALUES (4, 10, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (10, 10, TRUE);
+VALUES (10, 10, 'true');
 INSERT INTO attendees (attendee_id, game_night_id, attend_status)
-VALUES (12, 10, TRUE);
+VALUES (12, 10, 'true');
