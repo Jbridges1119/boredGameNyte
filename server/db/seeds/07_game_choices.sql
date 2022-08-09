9d01e5a3884e8954ac63c2e4f0de3407166437ce
Kenny Moon <kennymoon90@gmail.com>
2022-08-09 10:39:58 -0700

    Add seed data

---
+++ server/db/seeds/07_game_choices.sql
@@ -0,0 +1,61 @@
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (1, 'OIXt3DmJU0');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (1, 'EL3YmDLY6W');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (1, 'Ad9NDUFxmt');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (2, '3RVcHxhPEZ');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (2, 'oGVgRSAKwX');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (2, 'RLlDWHh7hR');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (3, '3RVcHxhPEZ');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (3, 'Ad9NDUFxmt');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (3, 'i5Oqu5VZgP');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (4, 'OIXt3DmJU0');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (4, 'axOr9uuOwN');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (4, 'i5Oqu5VZgP');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (5, 'EL3YmDLY6W');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (5, '3RVcHxhPEZ');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (5, '5H5JS0KLzK');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (6, '5H5JS0KLzK');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (6, 'axOr9uuOwN');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (6, 'OIXt3DmJU0');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (7, 'EL3YmDLY6W');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (7, 'OIXt3DmJU0');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (7, '3RVcHxhPEZ');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (8, 'OIXt3DmJU0');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (8, 'axOr9uuOwN');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (8, 'Z6h8zF22uh');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (9, 'WVgc4bUL8N');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (9, 'OF145SrX44');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (9, '3RVcHxhPEZ');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (10, 'oGVgRSAKwX');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (10, 'Ad9NDUFxmt');
+INSERT INTO game_choices (game_night_id, bgatlas_game_id)
+VALUES (10, '3RVcHxhPEZ');
+
