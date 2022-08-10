79f0b8adc82fd14fed1c4fbee057a8d46c31ce61
Jesse Kopala <jdkopala@gmail.com>
2022-08-10 12:23:49 -0600

    Fix errors in seeds and scheme

--- server/db/schema/07_game_choices.sql
+++ server/db/schema/07_game_choices.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS game_choices CASCADE;
 CREATE TABLE game_choices (
   id SERIAL PRIMARY KEY NOT NULL,
   game_night_id INTEGER REFERENCES game_nights(id) ON DELETE CASCADE,
-  bgatlas_game_1 VARCHAR(255) NOT NULL
-  bgatlas_game_2 VARCHAR(255) NOT NULL
-  bgatlas_game_3 VARCHAR(255) NOT NULL
+  bgatlas_game_1 VARCHAR(255) NOT NULL,
+  bgatlas_game_2 VARCHAR(255),
+  bgatlas_game_3 VARCHAR(255)
 );
