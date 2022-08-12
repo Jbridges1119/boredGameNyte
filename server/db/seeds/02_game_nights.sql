f8874410b63f9f83517d6bfe12e6262118138f62
Jeff <jbridges1119@gmail.com>
2022-08-12 14:26:05 -0600

    Reseeded db

--- server/db/seeds/02_game_nights.sql
+++ server/db/seeds/02_game_nights.sql
@@ -1,20 +1,20 @@
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'Game Night at Jeff\'s', 1, FALSE, 'complete', E'Jeff\'s backyard', '2022-08-25T08:00:53.000Z');
+VALUES (E'Game Night at Jeff\'s', 1, FALSE, 'complete', E'Jeff\'s backyard', '2022-08-25T14:30:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'Catan Night at Jeff\'s', 1, TRUE, 'complete', E'Jeff\'s livingroom', '2022-08-16T08:00:53.000Z');
+VALUES (E'Catan Night at Jeff\'s', 1, TRUE, 'complete', E'Jeff\'s livingroom', '2022-06-16T22:00:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'Newbie Night at Jeff\'s', 1, FALSE, 'complete', E'Jeff\'s kitchen', '2022-08-18T08:00:53.000Z');
+VALUES (E'Newbie Night at Jeff\'s', 1, FALSE, 'complete', E'Jeff\'s kitchen', '2022-08-18T08:45:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'Game Night at Jesse\'s', 3, FALSE, 'complete', E'Jesse\'s house', '2022-08-10T08:00:53.000Z');
+VALUES (E'Game Night at Jesse\'s', 3, FALSE, 'complete', E'Jesse\'s house', '2022-08-10T011:00:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('Lords of Waterdeep Game Night', 3, TRUE, 'complete', E'The Ogre\'s Den', '2022-08-13T08:00:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'What\'s Canadas Favorite board game? Sorry!', 3, FALSE, 'complete', '2022-08-13T08:00:53.000Z');
+VALUES (E'What\'s Canadas Favorite board game? Sorry!', 3, FALSE, 'complete', '2022-06-02T14:00:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('A Host with no Clue!', 3, TRUE, 'Scheduled', 'a location TBD', '2022-08-13T08:00:53.000Z');
+VALUES ('A Host with no Clue!', 3, TRUE, 'Scheduled', 'a location TBD', '2022-07-20T08:45:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES (E'Game Night at Kenny\'s', 2, TRUE, 'complete', E'Kenny\'s house', '2022-08-13T08:00:53.000Z');
+VALUES (E'Game Night at Kenny\'s', 2, TRUE, 'complete', E'Kenny\'s house', '2022-08-11T10:15:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
-VALUES ('Risk Takers', 2, TRUE, 'complete', 'Mind Games in Burnaby', '2022-08-13T08:00:53.000Z');
+VALUES ('Risk Takers', 2, TRUE, 'complete', 'Mind Games in Burnaby', '2022-08-29T16:30:53.000Z');
 INSERT INTO game_nights (title, host_id, competitive, status, location, date)
 VALUES ('A Night of Life', 2, FALSE, 'scheduled', E'Kenny\'s backyard', '2022-08-13T08:00:53.000Z');
