2628365872265ca89e2a5b53c11dc92c6d30fd52
Jesse Kopala <jdkopala@gmail.com>
2022-08-14 10:18:26 -0600

    Fix a line in addtocollection function

--- clients/src/hooks/useApplicationData.js
+++ clients/src/hooks/useApplicationData.js
@@ -163,7 +163,7 @@ const useApplicationData = () => {
     return axios.post(`http://localhost:3005/api/users/${user}/collection`, newGame )
     .then(() => {
       setState((prev) => {
-        return {...prev, collection: [...state.collection, newGame], globalCollection: [...state.globalCollection, newGame]}
+        return {...prev, collection: [...prev.collection, newGame], globalCollection: [...prev.globalCollection, newGame]}
       })
     })
     .catch((err) => {
